736ffa0ef9f90b6a94b0d5dcd4d092db975a1450
Vladislav Raspopin <fixer400@yandex.ru>
2024-04-06 23:49:36 +0300

    server cors init

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/api/hello", (req, res) => {
+    res.json({message:"Hello World!", peoples: ["John", "Jane", "Joe"]});
 });
 
 app.listen(PORT, () => {
